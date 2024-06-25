fdaa0aa2685a543c50913c6992c9e821daf23df1
anand-nandz <150254910+anand-nandz@users.noreply.github.com>
2024-06-25 14:57:06 +0530

    trie added

--- src/main.jsx
+++ src/main.jsx
@@ -7,13 +7,13 @@ import { UserContextProvider } from "./Storage/userContext.jsx";
 import firebaseApp from "./firebase/firebase.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // <React.StrictMode>
+  <React.StrictMode>
     <FirebaseContext.Provider value={firebaseApp}>
     <UserContextProvider>
       <App/>
     </UserContextProvider>
   </FirebaseContext.Provider>
 
-  // </React.StrictMode>
+  </React.StrictMode>
   
 );
